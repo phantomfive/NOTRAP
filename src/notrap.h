bf8dee4348c5f8556506b74410c1a6a5f2682b2a
Andrew Thompson <andrewthompson10@gmail.com>
2013-05-23 09:11:07 -0700

    added malloc and string stugg

--- src/notrap.h
+++ src/notrap.h
@@ -7,29 +7,58 @@
  * available, though for some platforms we use macros since there
  * are not huge differences.
  *
+ * It's best to keep this as small as possible, because then porting to
+ * new platforms is easier.
+ *
  * We use #ifdefs to make sure code only gets compiled for the various
  * platforms.
- * For code that is defined for OSX, we use #ifdef NOP_OSX
- * For code that is defined for Windows, we use #ifdef NOP_WOE
- * For code that is defined for Linux, we use #ifdef NOP_LIN
+ * For code that is defined for OSX, we use #ifdef NTP_OSX
+ * For code that is defined for Windows, we use #ifdef NTP_WOE
+ * For code that is defined for Linux, we use #ifdef NTP_LIN
  *
  * Copyright 2013 Andrew Thompson. Usable under the GPL3.0 or later.
  ***********************************************************************/
 
 #ifdef __APPLE__
-#define NOP_OSX 1
+#define NTP_OSX 1
+#include "notrap_osx.h"
 #endif
 
+
+/***********************************************************************
+ * Section for constants, like BOOL
+ ***********************************************************************/
+#ifndef BOOL
+#define BOOL char
+#endif
+#ifndef TRUE
+#define TRUE  1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+/**********************************************************************
+ * Section for various standard functions.
+ * Believe it or not, some platforms don't even suppor the standard
+ * library exactly.
+ **********************************************************************/
+//various includes
+#ifdef NTP_OSX
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#define NTPmalloc malloc
+#define NTPstrlen strlen
+#define NTPmemcpy memcpy
+#define NTPstrcpy strcpy
+#endif
 
 /**********************************************************************
  * Section for Networking functions.
  * Networking is tough, because there are more differences here than
  * with most other types of functions.
  *********************************************************************/
-//various includes
-#ifdef NOP_OSX
-
-#endif
 
 
 
