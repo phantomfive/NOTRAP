ea486d058fe429a9d35b17db72306a79031dd11a
Andrew Thompson <andrewthompson10@gmail.com>
2013-07-09 20:08:13 -0700

    Finished connect, and boy was it brutal

--- src/notrap_posix_sockets.c
+++ src/notrap_posix_sockets.c
@@ -1,13 +1,28 @@
 #include <notrap/notrap.h>
 #ifdef NTP_POSIX_THREADS
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 struct NTPSock_struct {
 	int sock;
 	char destination[5000];
-	BOOL error;
+	int  port;
 
-	//Indicates the connect thread is running
+	//errMsg holds the most recent error in a human
+	//readable format
+	char errMsg[2000];
+
+	//Indicates the connect thread is running.
+	//No other thread than the connec thread 
+	//has a right to modify the NTPSock_struct 
+	//while this is true.
 	volatile BOOL doingConnect;
+
+
+	//True if there was an error while connecting
+	BOOL connectError;
 	
 	//indicates NTPDisconnect() was called while the connect
 	//thread was running
@@ -30,17 +45,82 @@ struct NTPSock_struct {
 //so we're going to simulate it with a single thread
 static void doLookupAndConnectInSeparateThread(void *obj) {
 	NTPSock *sock = (NTPSock*)obj;
+	struct addrinfo hints, *servinfo, *p;
+	char port[50];
 
-	
-	NTPAcquireLock(sock->connectLock);
-	sock->doingConnect = NO;
-	NTPReleaseLock(sock->connectLock);
-
-	//Check to see if our connect got interrupted.
-	//If it did, we need to cleanup ourselves.
-	if(sock->shouldInterruptConnect==YES) {
-		NTPDisconnect(&sock);
+	//Please Lord, may I never have to write one of these again.
+	//Here is where we actually do the lookup.
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	sprintf(port, "%d",sock->port);
+	if((rv = getaddrinfo(sock->destination, port, &hints, &servinfo))!=0){
+		//deal with error
+		snprintf(sock->errMsg, sizeof(sock->errMsg), "DNS lookup, %s",
+		         gai_strerror(rv));
+		sock->errMsg[sizeof(sock->errMsg)-1]=0;
+		goto ERR;
 	}
+	
+	//got the lookup, now get ourselves a socket
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if((sock->sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol))<0){
+			snprintf(sock->errMsg, sizeof(sock->errMsg), "sock() failed, %s",
+			           strerror(errno));
+			sock->errMsg[sizeof(sock->errMsg)-1]=0;
+			//we can try again until we run out of p->ai_next
+			continue;
+		}
+
+		//and if we got the socket, try to connect
+		if(connect(sockfd, p->ai_addr, p->ai_addrlen) <0) {
+			snprintf(sock->errMsg, sizeof(sock->errMsg), 
+			        "connect to %s failed, %s\n", sock->destination,strerror(errno));
+			sock->errMsg[sizeof(sock->errMsg)-1]=0;
+			close(sock->sock);
+			//we can try again until we run out of p->ai_next
+			continue;
+		}
+		
+		break; //connection successful! p has our value,
+		       //but if we don't have success, p will be NULL
+	}
+
+	//don't dereference 'p' after this
+	freeaddrinfo(servinfo);
+
+	if(p==NULL) {
+		//connection unsuccessful on all attempts!
+		//we already stored the error message of the last error
+		goto ERR;
+	}
+
+	//success! We are connected, but we still need to
+	//mark our connect as done
+	goto SIGNAL_CONNECTION_COMPLETE;
+
+ERR:
+	//Only run this line if there was an error. But the rest
+	//always needs to be run. If you have trouble with GOTOs, sorry.
+	//The alternative is still uglier.
+	sock->connectError = TRUE;	
+
+
+SIGNAL_CONNECTION_COMPLETE:
+	//Check to see if our connect got interrupted by a disconnect
+	//If it did, we need to cleanup ourselves.
+	NTPAcquireLock(sock->connectLock); {
+
+		//The 'sock' is guaranteed to not be freed until
+		//this is set to NO. And it can only be set while
+		//we hold this lock.
+		sock->doingConnect = NO;
+		if(sock->shouldInterruptConnect==YES) {
+			NTPDisconnect(&sock);
+			NTPReleaseLock(sock->connectLock);
+		}
+	
+	} NTPReleaseLock(sock->connectLock);
 }
 
 
